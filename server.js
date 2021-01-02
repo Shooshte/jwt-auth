38b4ba4f9bdb3837f67ba3abb92040b9b0f69cd1
Miha Šuśteršič <miha.sustersic@taia.io>
2021-01-02 14:03:56 +0100

    added mongoDB configuration, JWT configuration to .env.example, added middleware for verifying User signup, added user and user role DB models. Added connecting to DB and initial role creation upon server startup

--- server.js
+++ server.js
@@ -18,10 +18,45 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to jwt-auth." });
-});
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit();
+  });
+
+// adding intial user roles to the roles collection
+// TODO refactor initial DB setup into separate file
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      db.ROLES.forEach((role) => {
+        new Role({
+          name: role,
+        }).save((err) => {
+          if (err) {
+            console.log("error", err);
+          }
+
+          console.log(`Added ${role} to roles collection.`);
+        });
+      });
+    }
+  });
+}
 
 // set port, listen for requests
 const PORT = process.env.PORT;
