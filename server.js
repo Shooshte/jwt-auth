351bd78c4c40ed58fdace314e4db7f33560d4f94
Miha Šuśteršič <miha.sustersic@taia.io>
2021-01-02 19:04:29 +0100

    added signup, signin routes, added user auth routes

--- server.js
+++ server.js
@@ -58,6 +58,9 @@ function initial() {
   });
 }
 
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+
 // set port, listen for requests
 const PORT = process.env.PORT;
 
