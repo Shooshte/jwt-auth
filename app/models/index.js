34176aecadeb430679cb906408c1550ac4b0767b
Miha Šuśteršič <miha.sustersic@taia.io>
2021-01-04 17:22:56 +0100

    db baseUrl now uses test env variables when process.env.NODE_ENV is 'test'

--- app/models/index.js
+++ app/models/index.js
@@ -4,8 +4,13 @@ mongoose.Promise = global.Promise;
 const role = require("./role.model");
 const user = require("./user.model");
 
+const baseUrl =
+  process.env.NODE_ENV === "test"
+    ? `mongodb+srv://${process.env.TEST_DB_USERNAME}:${process.env.TEST_DB_PASSWORD}@${process.env.TEST_DB_HOST}/`
+    : `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/`;
+
 const db = {
-  baseUrl: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/`,
+  baseUrl,
   mongoose: mongoose,
   role,
   roles: ["user", "admin"],
